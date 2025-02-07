112645433026556e5976f512725905b3942bbb48
SerhiiKhyzhko <63621545+SerhiiKhyzhko@users.noreply.github.com>
2025-02-07 15:21:54 +0200

    Hands-on exercises #9

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package puppy
 
+var someText = "Вдалого полювання, Сталкере!"
+const PI = 3.14159
+
+func CirkuleSquere(r float64) float64{
+	return PI * (r * r)
+}
+
+func Messege() string {
+	return someText
+}
+
 func Bark() string {
 	return "Woof!"
 }
